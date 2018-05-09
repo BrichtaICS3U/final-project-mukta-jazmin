8345913c90fe3970c4a0587e9c91e7eb0c8f5e16
Mahima <Mahima@Mahimas-MacBook-Pro.local>
2018-05-09 08:52:11 -0400

    jkdsfsjd

--- GAME.py
+++ GAME.py
@@ -1,11 +1,13 @@
 import pygame, random
 from player_class import player
 from treasure_class import Treasure
+from enemy_class import enemy
 pygame.init()
 
 PURPLE = (255, 0, 255)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 speed = 1
 
@@ -16,12 +18,19 @@ size = (SCREENWIDTH, SCREENHEIGHT)
 screen= pygame.display.set_mode(size)
 pygame.display.set_caption("...'")
 
-all_sprites_list = pygame.sprite.Group()
-all_treasure_list = pygame.sprite.Group()
-
 playerplayer = player(PURPLE, 20, 30, 10)
 
 treasure1 = Treasure([100, 60])
+
+enemy1 = enemy (BLACK, 20, 30, 10)
+enemy1.rect.x = 500
+enemy1.rect.y = 500
+
+all_sprites_list = pygame.sprite.Group()
+all_treasure_list = pygame.sprite.Group()
+
+all_enemy_sprites= pygame.sprite.Group()
+all_enemy_sprites.add(enemy1)
 
 all_sprites_list.add(playerplayer)
 all_treasure_list.add(treasure1)
@@ -46,6 +55,11 @@ while carryOn:
 
                                 
 #GAME LOGIC
+    for enemy in all_enemy_sprites:
+        enemy.moveForward(speed)
+
+    all_sprites_list.update()
+    
     hit= pygame.sprite.spritecollide(playerplayer, all_treasure_list, True)
 
     if hit:
@@ -54,8 +68,6 @@ while carryOn:
 
 #DRAWING ON SCREEN
     screen.fill(WHITE)
-
-    pygame.draw.rect(screen, RED, [400, 400, 200, 200])
 
     all_sprites_list.draw(screen)
     all_treasure_list.draw(screen)
